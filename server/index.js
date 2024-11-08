a296a72af92e119652ea27cdc4f8b8c008e1e50a
zepe0 <aitor.zepe@gmail.com>
2024-11-08 21:49:48 +0100

    paguinacion

---
+++ server/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
+
+io.on("connection", (socket) => {
+  console.log("Usuario Conectado");
+
+  socket.on("disconnect", () => {
+    console.log("Usuario Desconectado");
+  });
+
+  socket.on("chat", (msg) => {
+    io.emit("chat", msg);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/client/index.html");
+});
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get("/admin", (req, res) => {
+  res.sendFile(process.cwd() + "/client/admin.html");
+});
+app.get("/tickets", (req, res) => {
+  res.sendFile(process.cwd() + "/client/tickets.html");
+});
+
+app.get("/users", (req, res) => {
+  res.sendFile(process.cwd() + "/client/usuarios.html");
+});
