30e74988f87fd69497531880ecfd11aa1b31d482
zepe0 <aitor.zepe@gmail.com>
2024-11-11 19:07:00 +0100

    add sql

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
+import { createClient } from "@libsql/client";
+process.loadEnvFile();
 
+// Renombrados //
 const port = process.env.PORT ?? 3000;
 
 const app = express();
@@ -10,21 +13,47 @@ const io = new Server(server, {
   connectionStateRecovery: {},
 });
 
-io.on("connection", (socket) => {
+const db = createClient({
+  url: "libsql://firm-katana-zepe0.turso.io",
+  authToken: process.env.DB_TOKEN,
+});
+
+//   TABLAS SQL //
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS msn (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT,
+    user TEXT
+  )
+`);
+
+// Conexión
+
+/// CHAT
+
+io.on("connection", async (socket) => {
   console.log("Usuario Conectado");
 
   socket.on("disconnect", () => {
     console.log("Usuario Desconectado");
   });
 
-  socket.on("chat", (msg) => {
-    io.emit("chat", msg);
+  // mensajes //
+  socket.on("chat", async (msg) => {
+    let result;
+    try {
+      result = await db.execute({
+        sql: "INSERT INTO msn (content) VALUES (:msn)",
+        args: { msn: msg },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+    io.emit("chat", msg, result.lastInsertRowid.toString());
   });
 });
 
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// PATH server
 
 app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/client/index.html");
@@ -42,4 +71,8 @@ app.get("/users", (req, res) => {
 });
 app.get("/newticket", (req, res) => {
   res.sendFile(process.cwd() + "/client/formclient.html");
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
