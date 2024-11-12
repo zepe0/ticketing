d93b4ef4fa09a1e5d8406b5a61e043bde40591a6
zepe0 <aitor.zepe@gmail.com>
2024-11-12 20:33:24 +0100

    "Removed console logs and whitespace in loadEnv function"

--- config/env.js
+++ config/env.js
@@ -2,15 +2,14 @@ import fs from "fs-extra";
 
 export function loadEnv() {
   // Verificar si el archivo .env existe
+  
   const envPath = "./.env"; // Ruta al archivo .env
 
   if (fs.existsSync(envPath)) {
-    // Si el archivo .env existe, cargarlo
+ 
     process.loadEnvFile(envPath);
     console.log(".env loaded");
-    if (process.env.NODE_ENV !== "production") {
-      console.log(".env loaded for development");
-    }
+   
   } else {
     console.log(".env file does not exist.");
   }
