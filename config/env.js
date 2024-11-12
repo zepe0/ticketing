3980a22638010468ca6eba16f1b53de5a20d72f6
zepe0 <aitor.zepe@gmail.com>
2024-11-12 20:31:49 +0100

    "Refactor env.js: reformatted code, removed unnecessary lines, and added missing newline at end of file"

--- config/env.js
+++ config/env.js
@@ -1,21 +1,19 @@
-import fs from 'fs-extra';
-
-
+import fs from "fs-extra";
 
 export function loadEnv() {
-    // Verificar si el archivo .env existe
-    const envPath = './.env'; // Ruta al archivo .env
-  
-    if (fs.existsSync(envPath)) {
-      // Si el archivo .env existe, cargarlo
-      process.loadEnvFile(envPath);
-      console.log(".env loaded");
-    } else {
-      console.log(".env file does not exist.");
-    }
-  
-    // Activar solo en modo desarrollo
+  // Verificar si el archivo .env existe
+  const envPath = "./.env"; // Ruta al archivo .env
+
+  if (fs.existsSync(envPath)) {
+    // Si el archivo .env existe, cargarlo
+    process.loadEnvFile(envPath);
+    console.log(".env loaded");
     if (process.env.NODE_ENV !== "production") {
       console.log(".env loaded for development");
     }
+  } else {
+    console.log(".env file does not exist.");
   }
+
+  // Activar solo en modo desarrollo
+}
